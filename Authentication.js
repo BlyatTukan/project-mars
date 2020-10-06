e80ac760cd82a33845c30a2fb44402cd616c8595
BlyatTukan <Overnex03@gmail.com>
2020-10-06 18:48:22 +0200

    Version 0.3a

---
+++ Authentication.js
@@ -0,0 +1,12 @@
+function login(){
+
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+      // Handle Errors here.
+   
+      window.location.assign("https://www.google.com/search?client=opera-gx&q=javascript+move+to+another+page&sourceid=opera&ie=UTF-8&oe=UTF-8");
+      // ...
+  
+  }
+  
